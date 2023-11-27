0b0089e3522a0c160768aba014992c217e343d25
Lassi Piironen <lsipii@kapsi.fi>
2023-11-27 14:55:40 +0200

    refactor: move mock data to a resource file

--- src/services/WorkPermitsService.ts
+++ src/services/WorkPermitsService.ts
@@ -1,16 +1,6 @@
 async function retrieveWorkPermitsData() {
   return {
-    permits: [
-      {
-        permitName: "Seasonal work certificate",
-        permitAccepted: true,
-        permitType: "A",
-        validityStart: "2023-11-07T00:00:00",
-        validityEnd: "2024-02-19T00:00:00",
-        industries: ["79.1", "79.9"],
-        employerName: "Staffpoint Oy",
-      },
-    ],
+    permits: await import("../data/work-permit-mocks.json"),
   };
 }
 
